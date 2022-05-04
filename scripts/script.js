ed26e4ada160ede7ed5736086b85efeeb37fd79b
Serhii Solianenko <sergeysolyanenko@gmail.com>
2022-05-04 20:33:02 +0300

    Code style changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,34 @@
 function numberRegex() {
-  let firstNum = +prompt();
-  let secondNum = +prompt();
-  if (firstNum && secondNum) {
-    console.log(firstNum.toString(secondNum));
-    numberRegex();
-  } else {
-    console.log("Некорректный ввод!");
-  }
+    const firstNum = +prompt();
+    const secondNum = +prompt();
+
+    if (firstNum && secondNum) {
+        console.log(firstNum.toString(secondNum));
+        numberRegex();
+    } else {
+        console.log('Некорректный ввод!');
+    }
 }
 numberRegex();
+
 function getNum() {
-  let firstNum = +prompt();
-  if (isNaN(firstNum)) {
-    console.log("Некорректный ввод!");
-    return;
-  }
-  let secondNum = +prompt();
-  if (isNaN(secondNum)) {
-    console.log("Некорректный ввод!");
-    return;
-  }
-  if (firstNum && secondNum) {
-    console.log(`Ответ: ${firstNum + secondNum}, ${firstNum / secondNum}`);
-    getNum();
-  }
+    const firstNum = +prompt();
+
+    if (isNaN(firstNum)) {
+        console.log('Некорректный ввод!');
+        return;
+    }
+
+    const secondNum = +prompt();
+
+    if (isNaN(secondNum)) {
+        console.log('Некорректный ввод!');
+        return;
+    }
+
+    if (firstNum && secondNum) {
+        console.log(`Ответ: ${firstNum + secondNum}, ${firstNum / secondNum}`);
+        getNum();
+    }
 }
 getNum();
