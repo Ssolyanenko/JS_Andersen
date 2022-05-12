d696b8f14311eddd6864805ad617e5d535ef5eb2
Serhii Solianenko <sergeysolyanenko@gmail.com>
2022-05-12 09:31:36 +0300

    fix

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ Array.prototype.filter2 = function (callback, thisArg) {
   const res = [];
 
   for (let i = 0; i < this.length; i++) {
-      if (callback.call(this, this[i], i, this)) {
-        res.push(this[i]);
-      }
+    if (callback.call(this, this[i], i, this)) {
+      res.push(this[i]);
     }
+  }
 
   return res;
-  }
+};
 
 function createDebounceFunction(func, ms) {
   let timeout;
