7da81ebadab6895842a89da654e9db0cda4e07d2
Serhii Solianenko <sergeysolyanenko@gmail.com>
2022-05-17 13:27:30 +0300

    fix homeWork

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
-class Node{
-    constructor(data,prev) {
-        this.data = data;
-        this.prev = prev;
+
+class Stack {
+  constructor(maxSize) {
+    this.items = [];
+    if (maxSize !== null) {
+      this.maxSize = maxSize;
+    } else {
+      this.maxSize = 10;
     }
+  }
+
+  push(element) {
+    if (this.items.length === this.maxSize) {
+      throw new Error("Ошибка!");
+    } else {
+      this.items.push(element);
+    }
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      throw new Error("Ошибка!");
+    } else {
+      return this.items.pop();
+    }
+  }
+
+  peek() {
+    if (this.items.length === 0) {
+      return null;
+    } else {
+      return this.items[this.items.length - 1];
+    }
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+  toArray() {
+    return this.items;
+  }
+
+  static fromIterable(iterable) {
+    if (iterable[Symbol.iterator] === undefined) {
+      throw new Error("Ошибка!");
+    }
+
+    let size = 0;
+
+    for (let item of iterable) {
+      size++;
+    }
+
+    const stack = new Stack(size);
+
+    for (let item of iterable) {
+      stack.push(item);
+    }
+
+    return stack;
+  }
 }
 
-class Stack  {
-constructor() {
-    this.last =null
-}
-push(data){
-    this.last = new Node(data, this.last)
-}
-pop(){
-    let result;
-    if(this.last !== null){
-        result = this.last.data
-        this.last = this.last.prev
-    }
-    return result
-}
-}
-const stack = new Stack()
-stack.push(1)
-stack.push(2)
-stack.pop()
-stack.pop()
+module.exports = { Stack };
